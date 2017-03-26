7575f905fdc3d01a3fc10ef91c5fea61b7ec75ed
Zach <zengle7@gmail.com>
2017-03-26 16:31:51 -0500

    26

--- ProcessedCSVwDataGraphs.py
+++ ProcessedCSVwDataGraphs.py
@@ -10,16 +10,21 @@ from matplotlib import pyplot as plt
 
 # Import data
 sheet_title = 'AB02.pbo.nam08' # the excel file has a tab "Renland d180
-data = pd.read_csv('C:\\Users\\Zach\\My Documents\\College\\Superman Spring\\comp methods in geo\\Plate-Boundary-GPS-data\\AB02.pbo.nam08.csv', \
+data = pd.read_csv('..\\..\\..\\..\\College\\Superman Spring\\comp methods in geo\\Plate-Boundary-GPS-data\\AB02.pbo.nam08.csv', \
                       header=11, parse_dates=[0], infer_datetime_format=True)
                       # the last call turns the string of dates into a solid value
                       # pulls in a spreadsheet from the data folder in finder
+                      # header11 means column variables listed in row 12
+                      
+                      # the dots at the beginning of the call are for
+                      # the hard drive (C:), then the 3 folders you must open
+                      # to get to comp methods in geo
 
 # Give headers better names
 data.columns = ['date [m/d/y]', 'X Mvmt [mm]', 'Y Mvmt [mm]','Z Mvmt [mm]','' ,'' ,'' ,'' ]
 
 
-plt.figure(figsize=(12, 12)) 
+plt.figure(figsize=(16, 12)) 
 plt.plot(data['date [m/d/y]'], data['X Mvmt [mm]'])
 plt.title('PBO GPS X Movement at AB02, Nikolski, AK', fontsize=16, fontweight='bold')
 plt.xlabel('date [m/d/y]', fontsize=16)
@@ -27,7 +32,7 @@ plt.ylabel('X Mvmt [mm]', fontsize=16)
 plt.tight_layout()
 plt.show()
 
-plt.figure(figsize=(12, 12)) 
+plt.figure(figsize=(16, 12)) 
 plt.plot(data['date [m/d/y]'], data['Y Mvmt [mm]'])
 plt.title('PBO GPS Y Movement at AB02, Nikolski, AK', fontsize=16, fontweight='bold')
 plt.xlabel('date [m/d/y]', fontsize=16)
@@ -35,7 +40,7 @@ plt.ylabel('Y Mvmt [mm]', fontsize=16)
 plt.tight_layout()
 plt.show()
 
-plt.figure(figsize=(12, 12)) 
+plt.figure(figsize=(16, 12)) 
 plt.plot(data['date [m/d/y]'], data['Z Mvmt [mm]'])
 plt.title('PBO GPS Z Movement at AB02, Nikolski, AK', fontsize=16, fontweight='bold')
 plt.xlabel('date [m/d/y]', fontsize=16)
